1d502b46ef1882980967e27435577660bea1b25a
Nate Craun <nate@natecraun.net>
2015-03-19 21:15:30 -0400

    Node defs.

---
+++ src/node_defs.h
@@ -0,0 +1,30 @@
+/*
+    Copyright (C) 2014-2015 Nate Craun
+
+    This file is part of Jubal.
+
+    Jubal is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Jubal is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Jubal.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef JUBAL_NODE_DEFS_H
+#define JUBAL_NODE_DEFS_H
+
+#include "engine.h"
+
+extern AudioDef def_disabled;
+extern AudioDef def_sine;
+extern AudioDef def_fm;
+extern AudioDef def_sub;
+
+#endif
